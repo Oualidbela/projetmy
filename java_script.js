4a50c5b9942a6260912aa2dba7e1269e6518545b
Oualidbela <88989864+Oualidbela@users.noreply.github.com>
2021-08-23 22:05:32 +0100

    Add files via upload

---
+++ java_script.js
@@ -0,0 +1,13 @@
+/*javaSecript prencipal*/
+/*------------------------------------------------------*/
+
+
+//si passe la souris sur la photo
+function monphoto_ps() {
+        document.getElementById("m_img").style="width: 100%; height: 90%;border-radius:0px;margin: 0;transition: all 1s ease-in-out;"
+}
+//lorsque la souris "sort" sur la photo
+function monphoto_ss() {
+    document.getElementById("m_img").style="width: 200px;height: 200px;border-radius: 100px;margin-left: 102px;margin-top: 146px;"
+
+}
